b4cbcfe2b175a8f1a630f3f627d7ae69c717c382
Pawan <pawansonu4@gmail.com>
2019-03-31 12:15:39 +0530

    Initial commit

---
+++ thermos/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_moment import Moment
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Configure database
+app.config['SECRET_KEY'] = '~t\x86\xc9\x1ew\x8bOcX\x85O\xb6\xa2\x11kL\xd1\xce\x7f\x14<y\x9e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+app.config['DEBUG'] = True
+db = SQLAlchemy(app)
+
+# Configure authentication
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
+# for displaying timestamps
+moment = Moment(app)
+
+import models
+import views
